048f2e634a91701d6bbb8c8d6f39e7c79fb3dfcb
noureldinmahmoud <noureldinmahmoud02@gmail.com>
2024-01-31 12:13:54 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.noureldin.todoapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.noureldin.todoapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")  // Use the same version as other AndroidX libraries
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")  // Use the same version as other AndroidX libraries
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //LeakCanary
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.13")
+
+    //swipe to delete library
+    implementation("com.github.zerobranch:SwipeLayout:1.3.1")
+
+    //calendar
+    implementation("com.github.prolificinteractive:material-calendarview:2.0.0")
+
+
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+}
---
+++ app/src/main/java/com/noureldin/todoapp/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.noureldin.todoapp.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noureldin.todoapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
